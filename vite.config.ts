ac2cdec09e3b80fe9adc7323a65052f414dea645
ml520qingyin <ml1553441826@outlook.com>
2025-12-08 06:08:36 +0800

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,8 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      // GitHub Pages配置
+      base: '/magic/'
     };
 });
